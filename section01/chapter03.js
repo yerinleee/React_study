d7088effaafb4c6c31dab9512246d3583f7e3a9b
DESKTOP-7SQJT9E\c573 <yerinlee.yr@gmail.com>
2025-09-03 12:42:11 +0900

    250902

---
+++ section01/chapter03.js
@@ -0,0 +1,9 @@
+/*
+# Korean Language Pack for Visual Studio Code
+# Prettier - code formatter 설치 후 Ctrl+ , 키 눌러서 설정 들어간 다음에 Editor: Format On Save 체크박스에 체크하기 Editor: Default Formatter 에 Prettier 지정하기
+# Material Icon Theme - 파일 아이콘 직관적으로 보기위해서 설치, 파일 아이콘 테마 설정하기
+# Error Lens - 설치
+# Live Server - html 페이지를 만들고 ctrl + shift + p 누르고 open with live server 
+*/
+
+console.log("Hello, World");
