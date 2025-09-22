1f753f4c8e09cfb8dbfea5167b28f118ef09e0c2
DESKTOP-7SQJT9E\c573 <yerinlee.yr@gmail.com>
2025-09-22 16:45:23 +0900

    250916

---
+++ section05/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
