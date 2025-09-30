cc38e46ab10a4fd7742ef06e1bc6197bba976637
DESKTOP-7SQJT9E\c573 <yerinlee.yr@gmail.com>
2025-09-30 11:13:01 +0900

    250923

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/footer.jsx";
+import Button from "./components/Button.jsx";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <div>자식 요소</div>
+      </Button>
     </>
   );
 }
