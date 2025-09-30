69c6a0b0ae0f1d2ef0af01e6fd911015461402a1
DESKTOP-7SQJT9E\c573 <yerinlee.yr@gmail.com>
2025-09-30 15:23:22 +0900

    250930

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,14 @@
+import "./App.css";
 import { useState } from "react";
 
+import Bulb from "./components/Bulb";
+import Counter from "./components/Bulb";
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
-  console.log(count); // 현재 값 (0)
-  // setCount는 count를 변경하는 함수
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <p>현재 카운트: {count}</p>
-        <button onClick={() => setCount(count + 1)}>+1</button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
---
+++ section05/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>현재 카운트: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+};
+
+export default Counter;
