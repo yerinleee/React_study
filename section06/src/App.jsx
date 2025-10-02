ad8443c923b8bd6a9247ea447fb29a1d46571711
DESKTOP-7SQJT9E\c573 <dankyul2304@naver.com>
2025-10-02 13:47:16 +0900

    251002

---
+++ section06/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Contoroller";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
+}
+
+export default App;
